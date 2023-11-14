9dc9fefb25e188f52474641777181e4e0e903366
Jokwanhee <jonotch1@naver.com>
2023-11-14 09:45:40 +0900

    [add] 스터디 실습 3주차 만족도 조사 앱 구현

---
+++ Week3/app/src/main/java/com/example/week3/Canvas.kt
@@ -0,0 +1,78 @@
+package com.example.week3
+
+import android.graphics.Paint
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun RainbowCanvas(starNumber: MutableState<Int>) {
+    val animatedValue = remember { Animatable(0f) }
+    val starMax = 25f
+
+    // 특정 값으로 색을 채우는 Animation
+    LaunchedEffect(animatedValue) {
+        animatedValue.animateTo(
+            targetValue = 1f,
+            animationSpec = tween(durationMillis = 1000, easing = LinearEasing),
+        )
+    }
+
+    Canvas(
+        modifier = Modifier
+            .size(320.dp)
+            .padding(16.dp)
+    ) {
+        drawArc(
+            color = Color.LightGray,
+            startAngle = 180f,
+            sweepAngle = 180f,
+            useCenter = false,
+            size = Size(800.dp.value, 800.dp.value),
+            style = Stroke(width = 20f, cap = StrokeCap.Round)
+        )
+
+        drawArc(
+            color = Color.Red,
+            startAngle = 180f,
+            sweepAngle = (180f * starNumber.value) / starMax, // 180 : n = 전체 별점 : 내 별점 (n = 그림에서 나타나는 퍼센트)
+            useCenter = false,
+            size = Size(800.dp.value, 800.dp.value),
+            style = Stroke(width = 20f, cap = StrokeCap.Round)
+        )
+
+        drawContext.canvas.nativeCanvas.drawText(
+            "지금 내 점수는",
+            225f,
+            200f,
+            Paint().apply {
+                textSize = 64f
+                color = 0xff625b71.toInt()
+            }
+        )
+        drawContext.canvas.nativeCanvas.drawText(
+            "${((starNumber.value / starMax) * 100).toInt()}",  // (180 : n = 전체 별점 : 내 별점) * 100, 백분율 때문에 100 곱하기
+            365f,
+            350f,
+            Paint().apply {
+                textSize = 100f
+                color = 0xffff0000.toInt()
+            }
+        )
+
+    }
+}
--- Week3/app/src/main/java/com/example/week3/MainActivity.kt
+++ Week3/app/src/main/java/com/example/week3/MainActivity.kt
@@ -1,23 +1,97 @@
 package com.example.week3
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpOffset
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import com.example.week3.ui.theme.Week3Theme
+import com.example.week3.ui.theme.Yellow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Week3Theme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    ParkDongMin()
+
+                }
             }
         }
+    }
+}
+
+@Composable
+fun ParkDongMin() {
+    val openDialog = remember { mutableStateOf(false) }
+    val starNumber = remember { mutableStateOf(0) }
+
+    when {
+        openDialog.value -> {
+            StarDialog(openDialog) {
+                starNumber.value = it
+                Log.e("로그", "$it")
+            }
+        }
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.size(10.dp))
+
+        Button(
+            onClick = { openDialog.value = !openDialog.value }
+        ) {
+            Text(text = "설문조사 하기")
+        }
+
+        Spacer(modifier = Modifier.size(10.dp))
+
+        RainbowCanvas(starNumber)
     }
 }
